7f7704aefee83a8981195224e506297d0dbc5a17
Võ Thành Công <76848500+V-T-Cong@users.noreply.github.com>
2023-09-23 13:27:02 +0700

    finish routes

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -1,56 +1,12 @@
 const express = require('express')
 const app = express()
-let {people} = require('./data')
+
+const people = require('./routes/people')
 
 app.use(express.static('./methods-public'))
 app.use(express.urlencoded({extended: false}))
 app.use(express.json())
-
-app.get('/api/people', (req, res) => {
-	res.status(200).json({success: true, data: people})
-})
-
-app.post('/api/people', (req, res) => {
-	const {name} = req.body
-	if (!name) {
-		return res.status(400).json({success: false, msg:'please provide name value'})
-	}
-	res.status(201).send({success: true, person: name})
-})
-
-app.post('/api/postman/people', (req, res) => {
-	const {name} = req.body
-	if (!name) {
-		return res.status(400).json({success: false, msg: 'provide name value'})
-	}
-	res.status(201).send({success: true, data: [...people, name]})
-})
-
-app.put('/api/people/:id', (req, res) => {
-	const {id} = req.params
-	const {name} = req.body
-	
-	const person = people.find((person) => person.id === Number(id))
-	
-	if (!person) {
-		return res.status(404).json({success: false, msg: `No person with id ${{id}}`})
-	}
-	const newPeople = people.map((person) => {
-		if (person.id === Number(id)) {
-			person.name = name
-		}
-		return person
-	})
-	res.status(200).json({success: true, data: newPeople})
-})
-
-app.delete('/api/people/:id', (req, res) => {
-	const person = people.find((people) = person.id === (Number(req.params.id)))
-	if (!person) {
-		return res.status(404).json({ success: false, msg: `Cann't find person with id ${req.params.id}`})
-	}
-	res.status(201).send({success: true, person: name})
-})
+app.use('/api/people', people)
 
 app.post('/login', (req, res) => {
 	const {name} = req.body
@@ -63,4 +19,3 @@ app.post('/login', (req, res) => {
 app.listen(5000, () => {
 	console.log('Server is listening on port 5000....')
 })
-
